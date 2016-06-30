b19514255272f1036dbfc3d1cfe89b31fe21a24a
Evan Wildenhain <mew2@princeton.edu>
2016-06-30 09:34:25 -0700

    Updated TimeFormatter code

--- app/src/main/java/com/codepath/apps/mysimpletweets/time/TimeFormatter.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/time/TimeFormatter.java
@@ -8,8 +8,13 @@ import java.util.Locale;
 import java.util.TimeZone;
 
 /**
- * Created by evanwild on 6/29/16.
+ * Given a date String of the format given by the Twitter API, returns a display-formatted
+ * String representing the relative time difference, e.g. "2m", "6d", "23 May", "1 Jan 14"
+ * depending on how great the time difference between now and the given date is.
+ * This, as of 2016-06-29, matches the behavior of the official Twitter app.
  */
+
+/*
 public class TimeFormatter {
     public static String getTimeDifference(String pDate) {
         String time = "";
@@ -46,7 +51,7 @@ public class TimeFormatter {
                     } else {
                         time = String.valueOf(then.get(Calendar.DAY_OF_MONTH)) + " "
                                 + then.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US)
-                                + " " + then.get(Calendar.YEAR);
+                                + " " + String.valueOf(then.get(Calendar.YEAR) - 2000);
                     }
                 }
             } else {
@@ -70,10 +75,47 @@ public class TimeFormatter {
                 }
             }
         } catch (ParseException e) {
-            // System.out.println("Err: " + e);
             e.printStackTrace();
         }
         return time;
     }
 
+} */
+
+public class TimeFormatter {
+    public static String getTimeDifference(String rawJsonDate) {
+        String time = "";
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat format = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        format.setLenient(true);
+        try {
+            long diff = (System.currentTimeMillis() - format.parse(rawJsonDate).getTime()) / 1000;
+            if (diff < 5)
+                time = "Just now";
+            else if (diff < 60)
+                time = String.format(Locale.ENGLISH, "%ds",diff);
+            else if (diff < 60 * 60)
+                time = String.format(Locale.ENGLISH, "%dm", diff / 60);
+            else if (diff < 60 * 60 * 24)
+                time = String.format(Locale.ENGLISH, "%dh", diff / (60 * 60));
+            else if (diff < 60 * 60 * 24 * 30)
+                time = String.format(Locale.ENGLISH, "%dd", diff / (60 * 60 * 24));
+            else {
+                Calendar now = Calendar.getInstance();
+                Calendar then = Calendar.getInstance();
+                then.setTime(format.parse(rawJsonDate));
+                if (now.get(Calendar.YEAR) == then.get(Calendar.YEAR)) {
+                    time = String.valueOf(then.get(Calendar.DAY_OF_MONTH)) + " "
+                            + then.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US);
+                } else {
+                    time = String.valueOf(then.get(Calendar.DAY_OF_MONTH)) + " "
+                            + then.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US)
+                            + " " + String.valueOf(then.get(Calendar.YEAR) - 2000);
+                }
+            }
+        }  catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return time;
+    }
 }
